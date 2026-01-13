1f8d8d7762eb710a31998e9621e9f586b6bb732b
Ängla Juliusson <angla02@icloud.com>
2026-01-13 13:46:47 +0100

    Startsidan - Innehåll och styling

--- dt193g-laboration3/src/views/HomeView.vue
+++ dt193g-laboration3/src/views/HomeView.vue
@@ -1,14 +1,41 @@
+<script setup>
+    import Banner from '../components/Banner.vue' // Hämta banner bilden
+</script>
+
 <template>
-    <section>
-      <Banner />
-      <h1>Mitt fritidsintresse</h1>
-      <p>
-        Kort information om konserter, musikintresse osv.
-      </p>
-    </section>
-  </template>
+    <main>
+      <Banner /> <!-- Inkludera banner-bilden-->
+      <div class="text">
+        <h2>Mitt fritidsintresse</h2>
+        <p>
+            Mitt stora fritidsintresse är att gå på konserter och uppleva live-musik på riktigt nära håll. 
+            Jag älskar konserter eftersom det finns en speciell energi i rummet när publiken och musikerna möts 
+            – något som inte går att få genom en skärm eller en inspelning. Musiken känns levande, 
+            och varje konsert är ett unikt tillfälle som skapar minnen för livet. 
+            Det är även ett sätt för mig att upptäcka nya artister, träffa likasinnade musikälskare och bara njuta av stunden.
+            På denna sida kan du ta del av olika konserter jag varit på.
+        </p>
+      </div>
+    </main>
+</template>
   
-  <script setup>
-  import Banner from '../components/Banner.vue'
-  </script>
-  
+<style scoped>
+    main {
+        justify-content: center;
+        align-items: center;
+    }
+    .text {
+        padding: 2em;
+        margin: 0 auto;
+        margin-top: 1em;
+        max-width:500px;
+    }
+    h2 {
+        color: rgb(56, 142, 107);
+    }
+    p {
+        margin-top: 1em;
+        margin-bottom: 3em;
+        line-height: 1.6;
+    }
+</style> 
