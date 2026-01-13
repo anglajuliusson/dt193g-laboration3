0cabdae46e915c995ed74d76fece62f503f21ab6
Ängla Juliusson <angla02@icloud.com>
2026-01-13 13:28:02 +0100

    Lägg in bilden i Banner.vue och addera styling

---
+++ dt193g-laboration3/src/components/Banner.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+  
+<template>
+    <picture>
+        <source srcset="../assets/images/banner.avif" type="image/avif">
+        <source srcset="../assets/images/banner.webp" type="image/webp">
+        <img src="../assets/images/banner.jpg" alt="Bannerbild - Kent konsert" class="banner-img">
+    </picture>
+</template>
+  
+<style scoped>
+    .banner-img {
+        width: 100%;
+        height: auto;
+        display: block;
+        object-fit: cover;
+    }
+</style>
