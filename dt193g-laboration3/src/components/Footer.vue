061e70926a33850b4e93c394b78250ed4f273737
Ängla Juliusson <angla02@icloud.com>
2026-01-13 12:24:29 +0100

    Footer med innehåll och styling

---
+++ dt193g-laboration3/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+  
+<template>
+    <footer>
+        <p>Ängla Juliusson</p>
+        <p>DT193G - Laboration 3</p>
+    </footer>
+</template>
+  
+<style scoped>
+    footer {
+        background-color: rgb(56, 142, 107);
+        color: white;
+        padding: 2em
+    }
+</style>
