b118f313830e6388cef27d3ab219a0da2e0d9654
Ängla Juliusson <angla02@icloud.com>
2026-01-13 13:02:49 +0100

    Lägg till router i main.js för att hantera SPA-navigation

--- dt193g-laboration3/src/main.js
+++ dt193g-laboration3/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "./assets/main.css"; // Inkludera CSS-fil
+import router from './router/index.js'; // Inkludera router
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router) // Gör att router fungerar
+    .mount('#app')
